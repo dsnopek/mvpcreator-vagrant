ec88c189210f5d6dc6df2437c11828171ca12d97
David Snopek <dsnopek@gmail.com>
2013-05-13 13:40:37 -0500

    Setup the 'puppet-mvpcreator' submodule.

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,17 +2,21 @@
 import "modules.pp"
 
 # Some Aegir overrides:
-$aegir_hostmaster_url = 'panel.mvpcreator.dev'
-$aegir_email = 'aegir@example.com'
+#$aegir_hostmaster_url = 'panel.mvpcreator.dev'
+#$aegir_email = 'aegir@example.com'
 #$aegir_force_login_link = true
 
-Exec {
-  path => '/usr/bin:/bin',
-}
+#Exec {
+#  path => '/usr/bin:/bin',
+#}
 
-include aegir
-include aegir::queue_runner
+#include aegir
+#include aegir::queue_runner
 
 include mvpcreator
-include mvpcreator::webserver
+
+class {'mvpcreator::webserver':
+  aegir_url => 'panel.mvpcreator.dev',
+  aegir_email => 'aegir@example.com',
+}
 
